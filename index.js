5de9a8cdf36f568f2e43fd1b66e71b3e65e9b6ff
Noble-Chief <146099476+Noble-Chief@users.noreply.github.com>
2023-09-26 20:21:42 +0530

    Update index.js
    Removed chacha bc

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const document_writer_profile= require("./models/document_writer_profile.js");
 const mediator_profile= require("./models/mediator_profile.js");
 const notary_profile= require("./models/notary_profile.js");
 
-const chacha="BC"
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
